9f2495e87fe758e520dadd46c812fa832c35d48a
em.colombo <em.colombo@1280f8de-3a3a-11de-be05-5f7a86268029>
2009-06-23 18:14:43 +0000

    Added GPL2 disclaimer at the top of all source files.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -5,6 +5,24 @@
 #include "oslib.h"
 
 #include <QtGui/QMessageBox>
+/* DUKTO - A simple, fast and multi-platform file transfer tool for LAN users
+ * Copyright (C) 2009 Emanuele Colombo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 #include <QtGui/QFileDialog>
 #include <QUrl>
 #include <QTime>
