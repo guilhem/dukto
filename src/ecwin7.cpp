0782fc475fa5a078d9784100f42d9cf7bcd01b2e
em.colombo@gmail.com <em.colombo@gmail.com@1280f8de-3a3a-11de-be05-5f7a86268029>
2010-08-10 17:55:38 +0000

    Added comments and license information to EcWin7

--- src/ecwin7.cpp
+++ src/ecwin7.cpp
@@ -1,25 +1,37 @@
+/* EcWin7 - Support library for integrating Windows 7 taskbar features
+ * into any Qt application
+ * Copyright (C) 2010 Emanuele Colombo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 #include "ecwin7.h"
 
-#include <QDebug>
-
+// Windows only GUID definitions
 #if defined(Q_WS_WIN)
-
-// GUID and IID definition
-//DEFINE_GUID(IID_IDropTargetHelper,0x4657278b,0x411b,0x11d2,0x83,0x9a,0x00,0xc0,0x4f,0xd9,0x18,0xd0);
-//DEFINE_GUID(IID_IPersistFolder2,0x1ac3d9f0,0x175c,0x11d1,0x95,0xbe,0x00,0x60,0x97,0x97,0xea,0x4f);
-//DEFINE_GUID(IID_IShellFolder2,0x93f2f68c,0x1d1b,0x11d3,0xa3,0x0e,0x00,0xc0,0x4f,0x79,0xab,0xd1);
-//DEFINE_GUID(IID_IPersistFolder3,0xcef04fdf,0xfe72,0x11d2,0x87,0xa5,0x00,0xc0,0x4f,0x68,0x37,0xcf);
 DEFINE_GUID(CLSID_TaskbarList,0x56fdf344,0xfd6d,0x11d0,0x95,0x8a,0x0,0x60,0x97,0xc9,0xa0,0x90);
-//DEFINE_GUID(IID_ITaskbarList,0x56fdf342,0xfd6d,0x11d0,0x95,0x8a,0x0,0x60,0x97,0xc9,0xa0,0x90);
 DEFINE_GUID(IID_ITaskbarList3,0xea1afb91,0x9e28,0x4b86,0x90,0xE9,0x9e,0x9f,0x8a,0x5e,0xef,0xaf);
-
 #endif
 
+// Constructor: variabiles initialization
 EcWin7::EcWin7()
 {
     mOverlayIcon = NULL;
 }
 
+// Init taskbar communication
 void EcWin7::init(WId wid)
 {
     mWindowId = wid;
@@ -28,6 +40,8 @@ void EcWin7::init(WId wid)
 #endif
 }
 
+// Windows event handler callback function
+// (handles taskbar communication initial message)
 bool EcWin7::winEvent(MSG * message, long * result)
 {
 #ifdef Q_WS_WIN
@@ -45,6 +59,7 @@ bool EcWin7::winEvent(MSG * message, long * result)
     return false;
 }
 
+// Set progress bar current value
 void EcWin7::setProgressValue(int value, int max)
 {
 #ifdef Q_WS_WIN
@@ -52,6 +67,7 @@ void EcWin7::setProgressValue(int value, int max)
 #endif
 }
 
+// Set progress bar current state (active, error, pause, ecc...)
 void EcWin7::setProgressState(ToolBarProgressState state)
 {
 #ifdef Q_WS_WIN
@@ -59,6 +75,8 @@ void EcWin7::setProgressState(ToolBarProgressState state)
 #endif
 }
 
+// Set new overlay icon and corresponding description (for accessibility)
+// (call with iconName == "" and description == "" to remove any previous overlay icon)
 void EcWin7::setOverlayIcon(QString iconName, QString description)
 {
 #ifdef Q_WS_WIN
